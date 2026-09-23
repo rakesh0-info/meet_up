69d929a21fb8de7d39691201b4e7610756265fac
rakesh0-info <rakesh.kayal@technoexponent.co.in>
2026-09-23 18:18:25 +0530

    fix: add start cmd1

--- Backend_Meet/start.sh
+++ Backend_Meet/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+
 # Start Celery worker in the background
 celery -A celery_con.celery_config:celery_app worker --loglevel=info --pool=solo &
 
-# Start your web application (replace with your actual web server command, e.g., gunicorn or uvicorn)
-gunicorn myproject.wsgi:application --bind 0.0.0.0:$PORT
+# Start your FastAPI/Uvicorn application
+uvicorn main:app --host 0.0.0.0 --port $PORT
